9f585de838e856de0a352beda16ee1cbc4d999f1
Alexey <oleksii.kuts@faceit-team.com>
2022-02-16 10:53:41 +0200

    Refactoring code

--- soccer.rb
+++ soccer.rb
@@ -1,24 +1,23 @@
 # convert score to ranking points
 def ranking(score)
   if score[0] == score[1]
-    points = [1, 1]
+    [1, 1]
   elsif score[0] > score [1]
-    points = [3, 0]
+    [3, 0]
   else
-    points = [0, 3]
+    [0, 3]
   end
 end
 
 # read lines from stdin
-def get_results
+def read_results
   results = []
-# Капец! А шо, так можно было!!!
-  while line = gets do
+  while (line = gets) # Капец! А шо, так можно было!!!
     arr = line.split(', ')
-    name = arr.map {|a| a.split[0..-2].join(' ')}
-    score = arr.map {|a| a.split[-1]}
+    name = arr.map { |a| a.split[0..-2].join(' ') }
+    score = arr.map { |a| a.split[-1] }
     points = ranking(score)
-    points.each_index {|i| results << [points[i], name[i]]}
+    points.each_index { |i| results << [points[i], name[i]] }
   end
   results
 end
@@ -28,9 +27,9 @@ def make_ranking_table(results)
   return until results
   results.each do |team|
     sum = 0
-    results.each {|v| sum += v[0] if team[1] == v[1]}
-    results.delete_if {|v| team[1] == v[1]}
-    results.unshift([-sum, team[1]])  # make negative and swap to proper sorting order
+    results.each { |v| sum += v[0] if team[1] == v[1] }
+    results.delete_if { |v| team[1] == v[1] }
+    results.unshift([-sum, team[1]]) # make negative and swap to proper sorting order
   end
   results.sort!
 end
@@ -38,9 +37,9 @@ end
 # output ranking table to stdout
 def print_ranking_table(results)
   return until results
-  results.each_index {|i| puts "#{i+1}. #{results[i][1]}, #{-results[i][0]}"}
+  results.each_index { |i| puts "#{i + 1}. #{results[i][1]}, #{-results[i][0]}" }
 end
 
-print_ranking_table(make_ranking_table(get_results))
+print_ranking_table(make_ranking_table(read_results))
 # 1. если нет параметров pipe or redirect, запросить имя файла
 # 2. анализ pipe or redirect or file_name
